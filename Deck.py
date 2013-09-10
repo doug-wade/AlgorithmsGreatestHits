86e7886edd0abe2c5ea6a17f763f49fdf857bba9
prekolna <prekolna@gmail.com>
2013-09-09 22:25:51 -0500

    Added heapsort

---
+++ Deck.py
@@ -0,0 +1,31 @@
+class Deck:
+    """
+    An implementation of a deck that allows pushing and popping from both the
+    front and the back of a doubly linked list.
+    """
+    def __init__():
+        """
+        Initializes an empty deck.
+        """
+        self._length = 0
+        self._first = None
+        self._last = None
+
+    class Node:
+        """
+        A single element of the linked list.
+        """
+        def __init__(self, value, next=None, prev=None):
+            self.value = value
+            self.next = next
+            self.prev = prev
+
+    def enqueue():
+
+    def dequeue():
+
+    def size():
+        """
+        Returns the number of elements in the deck.
+        """
+        return self._length
