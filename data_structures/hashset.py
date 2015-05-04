f34e6a5bfe9af58ec23895e265a566485415a753
Doug Wade <douglas.b.wade@gmail.com>
2015-05-03 23:39:14 -0700

    Add docs to hashset

--- data_structures/hashset.py
+++ data_structures/hashset.py
@@ -2,13 +2,17 @@ from hashlib import md5
 
 class HashSet:
     """
-    An implementation of a hash set.  Note that all sets contain None.
+    An implementation of a hash set.  Supports strings, ints, and any class that implements __hash__.
+    Note that all sets contain None.
     """
     def __init__(self):
         self.array = [None] * 8
         self.__size = 0
 
     def add(self, elem):
+        """
+        Add an element to the set.
+        """
         if self.array[self.__find_pos(elem)] != elem:
             self.array[self.__find_pos(elem)] = elem
             self.__size += 1
@@ -16,16 +20,28 @@ class HashSet:
                 self.__rehash()
 
     def add_all(self, elems):
+        """
+        Add all elements from an iterable to the set.
+        """
         for elem in elems:
             self.add(elem)
 
     def contains(self, elem):
+        """
+        Returns true if the element is in the set, false otherwise.  All sets are considered to contain None.
+        """
         return elem is None or self.array[self.__find_pos(elem)] is not None
 
     def size(self):
+        """
+        Returns the number of elements currently in the set.
+        """
         return self.__size
 
     def __find_pos(self, elem):
+        """
+        An internal method for determining the position for an element in the backing list.
+        """
         if elem is None:
             return 0
 
@@ -52,6 +68,9 @@ class HashSet:
         return position
 
     def __rehash(self):
+        """
+        An internal method for resizing the backing list.
+        """
         old_elems = self.array
         self.array = [None] * (len(old_elems)**2)
         self.__size = 0
